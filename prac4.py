bde493864e66f6b74021703c06f5ac6a1fe3ac81
Aadil-Eyasim <88938866+Aadil-Eyasim@users.noreply.github.com>
2021-10-08 19:01:52 +0200

    6775

--- prac4.py
+++ prac4.py
@@ -15,7 +15,6 @@ cs = digitalio.DigitalInOut(board.D5)
 # create the mcp object
 mcp = MCP.MCP3008(spi, cs)
 # create an analog input channel on pin 1
-
 chan1 = AnalogIn(mcp, MCP.P1) #for Temp sensor
 chan2 = AnalogIn(mcp, MCP.P2) #for LDR
 
@@ -24,51 +23,47 @@ chan2 = AnalogIn(mcp, MCP.P2) #for LDR
 
 toggle_btn = 23   
 def setup():
-    #setup button
+
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(toggle_btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.add_event_detect(toggle_btn, GPIO.FALLING, callback=toggle_btn_pressed, bouncetime=300) 
     pass
     
 
-t = 1
-print('{:<12s} {:<15s} {:<15s} {:<15s}'.format('Runtime','Temp Reading', 'Temp', 'Light Reading'))
-
- 
+t = 1 
 start = time.time()
 def print_runtime_temp_thread():
 
     thread = threading.Timer(t, print_runtime_temp_thread)
-    thread.daemon = True  # Daemon threads exit when the program does
+    thread.daemon = True  
     thread.start()
-
-    
+ 
     end = time.time() #get the end time 
     runtime = math.trunc(end-start) 
  
     temp = (chan1.voltage - 0.5)*100  
     
-    print('{:<12s} {:<15d} {:<4.1f} C {:>15d}'.format(str(runtime)+'s', chan1.value, temp, chan2.value))
+    print('{:<12s} {:<15d} {:<12s} C {:>15d}'.format(str(runtime)+'s', chan1.value, temp, chan2.value))
     pass 
 
-# sampling period button
-presses = 0
+
 def toggle_btn_pressed(toggle_btn):
-#this function increases the sampling period if the button is pressed
+
     global t
     if GPIO.event_detected(toggle_btn):
-        if t==1:
-            t=10
-        elif t==10:
+        if t=>5:
+            t=1
+        elif t==5:
             t=1
         else:
-            t=5
+            t=10
         return t
 pass
 
 if __name__ == "__main__":
-    setup() #call setup
-    print_runtime_temp_thread() # call it once to start the thread
+    setup() 
+    print('{:<12s} {:<15s} {:<12s} {:<15s}'.format('Runtime','Temp Reading', 'Temp', 'Light Reading'))
+    print_runtime_temp_thread() 
     
     # Tell our program to run indefinitely
     while True:
